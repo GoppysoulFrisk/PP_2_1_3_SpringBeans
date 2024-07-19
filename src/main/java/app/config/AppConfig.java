5ccca597950b94425709ff9c94973aa03394bdbc
CoreFrisk <martakova_aa@list.ru>
2024-07-19 13:13:54 +0300

    Complete(maybe)

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,23 @@
 package app.config;
 
+import app.model.Cat;
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean(name = "dog")
+    public Dog Dog() {
+        return new Dog();
+    }
+
+    @Bean(name = "cat")
+    public Cat cat() {
+        return new Cat();
+    }
 }
